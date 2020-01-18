e55112f999f9343143bb64df6f10d787edd2d1cc
cloudyan <1395093509@qq.com>
2020-01-18 22:51:19 +0800

    dev-8f7e6dc

--- config/index.js
+++ config/index.js
@@ -9,8 +9,8 @@
   // cdn地址 = 域名 + 版本号
   // window.SITE_CONFIG['domain'] = 'https://platform-wxmall.oss-cn-beijing.aliyuncs.com/boot/' // 域名
   // window.SITE_CONFIG['domain'] = './' // 域名
-  window.SITE_CONFIG['domain'] = 'https://static.cloudai.net/' // 域名
-  window.SITE_CONFIG['version'] = '2001182239'   // 版本号(年月日时分)
+  window.SITE_CONFIG['domain'] = 'http://static.cloudai.net/' // 域名
+  window.SITE_CONFIG['version'] = '2001182249'   // 版本号(年月日时分)
   window.SITE_CONFIG['cdnUrl'] = window.SITE_CONFIG.domain + window.SITE_CONFIG.version
 })()
 
